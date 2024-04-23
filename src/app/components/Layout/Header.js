e97be39f2531a821f2e39bfb675488e84ed8fcf3
AdrienLapasset <aflapasset@gmail.com>
2024-04-23 12:15:52 +0200

    add logo

--- src/app/components/Layout/Header.js
+++ src/app/components/Layout/Header.js
@@ -2,8 +2,8 @@ import HorsetedLogo from "@/assets/logos/HorsetedLogo";
 
 export default function Header() {
   return (
-    <>
+    <header className="container mx-auto py-5">
       <HorsetedLogo />
-    </>
+    </header>
   );
 }
