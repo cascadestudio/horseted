bc1832978cdf7ec04e564fc561500637f7960458
AdrienLapasset <aflapasset@gmail.com>
2024-04-23 15:04:26 +0200

    add search bar

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,10 +1,12 @@
 import HorsetedLogo from "@/assets/logos/HorsetedLogo";
 import Button from "@/components/Button";
+import SearchBar from "./SearchBar";
 
 export default function Header() {
   return (
     <header className="container mx-auto py-5 flex">
       <HorsetedLogo />
+      <SearchBar />
       <Button>Vendre</Button>
       <Button variant="white">Vendre</Button>
     </header>
