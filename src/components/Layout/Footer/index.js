6875dcfdab516ac9f8e9d3f82d31e8794fa621d5
Clément Lapasset <clement.lapasset@gmail.com>
2024-04-24 12:02:58 +0200

    init footer

---
+++ src/components/Layout/Footer/index.js
@@ -0,0 +1,11 @@
+import HorsetedBlackIcon from "@/assets/logos/HorsetedBlackIcon";
+import Button from "@/components/Button";
+
+export default function Footer() {
+  return (
+    <footer>
+      <HorsetedBlackIcon />
+      <Button className="">Vendre maintenant</Button>
+    </footer>
+  );
+}
