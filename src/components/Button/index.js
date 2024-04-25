8def934df131f49a9aa59a96c4f8c76a667a20de
Clément Lapasset <clement.lapasset@gmail.com>
2024-04-25 14:53:13 +0200

    add sections to footer

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,6 +4,8 @@ export default function Button({ children, variant, className }) {
       className={`${className} font-mcqueen font-semibold rounded-xl h-11 px-7 ${
         variant === "white"
           ? "bg-white text-dark-green border border-dark-green"
+          : variant === "black"
+          ? "bg-transparent text-black border border-black"
           : "bg-dark-green text-white"
       }`}
     >
--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -1,118 +1,192 @@
+import Image from "next/image";
 import HorsetedBlackIcon from "@/assets/logos/HorsetedBlackIcon";
 import Button from "@/components/Button";
 import InstagramIcon from "@/assets/icons/InstagramIcon";
 import YoutubeIcon from "@/assets/icons/YoutubeIcon";
 import FacebookIcon from "@/assets/icons/FacebookIcon";
 import LinkedInIcon from "@/assets/icons/LinkedInIcon";
+import PlayStoreButton from "@/assets/buttons/PlayStoreButton.svg";
+import AppStoreButton from "@/assets/buttons/AppStoreButton.svg";
+import BurstIcon from "@/assets/icons/BurstIcon.svg";
+import HorsetedApp from "@/assets/images/HorsetedApp.png";
+import RightArrow from "@/assets/icons/RightArrow.svg";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <>
-      <footer className="container mx-auto pt-20 px-11 lg:flex lg:justify-around lg:px-0">
-        <div className="flex flex-col mb-8">
-          <HorsetedBlackIcon className="h-24 self-center mb-3 lg:h-40 lg:mb-7" />
-          <Button className="px-16">Vendre maintenant</Button>
+      <footer>
+        <div className="flex flex-col items-center bg-light-grey px-5 pt-16 lg:pt-32 lg:px-64">
+          <h3 className="font-mcqueen font-bold text-3xl text-center mb-5 lg:text-4xl">
+            Téléchargez l’application Horseted
+          </h3>
+          <p className="font-mcqueen font-medium text-center text-lg mb-10 lg:px-7">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation
+          </p>
+          <Image priority src={HorsetedApp} alt="App Horseted" />
         </div>
-        <div className="pb-7">
-          <h4 className="uppercase font-extrabold pb-2 text-lg">Horseted</h4>
-          <ul>
-            <li className="pb-2">
-              <a href="#">À propos</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Aide</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Vendre</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Acheter</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Livraisons & retours</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Articles</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Écurie</a>
-            </li>
-          </ul>
+        <div className="hidden lg:block lg:bg-[#E8E5DB] lg:border-t lg:border-light-green">
+          <div className=" lg:container lg:mx-auto lg:flex lg:py-11 lg:justify-between lg:items-center lg:px-5">
+            <div>
+              <h3 className="font-mcqueen font-bold text-2xl text-center lg:text-4xl">
+                Vendez votre matériel dès maintenant
+              </h3>
+              <p>
+                Vendez gratuitement votre matériel d’équitation et gagnez de
+                l’argent dès maintenant !
+              </p>
+            </div>
+            <Button className="lg:flex lg:items-center" variant="black">
+              Vendre un équipement{" "}
+              <Image
+                className="lg:ml-3"
+                priority
+                src={RightArrow}
+                alt="Right Arrow"
+              />
+            </Button>
+          </div>
         </div>
-        <div className="pb-7">
-          <h4 className="uppercase font-extrabold pb-2 text-lg">
-            Notre sélection
-          </h4>
-          <ul>
-            <li className="pb-2">
-              <a href="#">Vêtements pour la cavalière</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Boots en cuir pour la cavalière</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Casques</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Sangles et accessoires</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Enrênements</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Selles et accessoires</a>
-            </li>
-            <li className="pb-2">
-              <a href="#">Brosses</a>
-            </li>
-          </ul>
+        <div className="flex flex-col items-center bg-light-grey border-t border-b border-light-green pb-14 pt-12 lg:pb-16 lg:pt-11">
+          <div className="flex flex-col mb-7 lg:mb-5">
+            <Image
+              className="self-end mr-3 lg:w-12 lg:-mr-20 lg:-mb-3"
+              priority
+              src={BurstIcon}
+              alt="Burst Icon"
+            />
+            <p className="text-center -mt-3 font-bold text-2xl lg:text-4xl">
+              Achetez et vendez <br />
+              votre matériel d’équitation
+            </p>
+          </div>
+          <div className="lg:flex lg:gap-6">
+            <a href="#">
+              <Image
+                className="pb-3 lg:pb-0"
+                priority
+                src={PlayStoreButton}
+                alt="Télécharger sur Play Store"
+              />
+            </a>
+            <a href="#">
+              <Image
+                priority
+                src={AppStoreButton}
+                alt="Télécharger sur App Store"
+              />
+            </a>
+          </div>
         </div>
-        <div className="lg:flex lg:flex-col lg:justify-between lg:pb-7">
-          <div>
-            <h4 className="uppercase font-extrabold pb-2 text-lg">Légal</h4>
-            <ul className="pb-9 lg:pb-0">
+        <div className="container mx-auto pt-20 px-11 lg:flex lg:justify-around lg:px-0">
+          <div className="flex flex-col mb-8">
+            <HorsetedBlackIcon className="h-24 self-center mb-3 lg:h-40 lg:mb-7" />
+            <Button className="px-16">Vendre maintenant</Button>
+          </div>
+          <div className="pb-7">
+            <h4 className="uppercase font-extrabold pb-2 text-lg">Horseted</h4>
+            <ul>
               <li className="pb-2">
-                <a href="#">Mentions légales</a>
+                <a href="#">À propos</a>
               </li>
               <li className="pb-2">
-                <a href="#">Conditions générales des ventes</a>
+                <a href="#">Aide</a>
               </li>
               <li className="pb-2">
-                <a href="#">Conditions d’utilisation</a>
+                <a href="#">Vendre</a>
               </li>
               <li className="pb-2">
-                <a href="#">Charte vendeur</a>
+                <a href="#">Acheter</a>
+              </li>
+              <li className="pb-2">
+                <a href="#">Livraisons & retours</a>
+              </li>
+              <li className="pb-2">
+                <a href="#">Articles</a>
+              </li>
+              <li className="pb-2">
+                <a href="#">Écurie</a>
               </li>
             </ul>
           </div>
-          <div className="flex justify-center lg:justify-start">
-            <div className="inline-flex justify-center items-center gap-5 px-5 py-3 bg-light-grey border border-black rounded-full lg:bg-transparent lg:border-none lg:px-0 lg:py-0 lg:pb-2">
-              <a href="#">
-                <InstagramIcon className="h-6" />
-              </a>
-              <a href="#">
-                <YoutubeIcon className="h-6" />
-              </a>
-              <a href="#">
-                <FacebookIcon className="h-6" />
-              </a>
-              <a href="#">
-                <LinkedInIcon className="h-6" />
-              </a>
+          <div className="pb-7">
+            <h4 className="uppercase font-extrabold pb-2 text-lg">
+              Notre sélection
+            </h4>
+            <ul>
+              <li className="pb-2">
+                <a href="#">Vêtements pour la cavalière</a>
+              </li>
+              <li className="pb-2">
+                <a href="#">Boots en cuir pour la cavalière</a>
+              </li>
+              <li className="pb-2">
+                <a href="#">Casques</a>
+              </li>
+              <li className="pb-2">
+                <a href="#">Sangles et accessoires</a>
+              </li>
+              <li className="pb-2">
+                <a href="#">Enrênements</a>
+              </li>
+              <li className="pb-2">
+                <a href="#">Selles et accessoires</a>
+              </li>
+              <li className="pb-2">
+                <a href="#">Brosses</a>
+              </li>
+            </ul>
+          </div>
+          <div className="lg:flex lg:flex-col lg:justify-between lg:pb-7">
+            <div>
+              <h4 className="uppercase font-extrabold pb-2 text-lg">Légal</h4>
+              <ul className="pb-9 lg:pb-0">
+                <li className="pb-2">
+                  <a href="#">Mentions légales</a>
+                </li>
+                <li className="pb-2">
+                  <a href="#">Conditions générales des ventes</a>
+                </li>
+                <li className="pb-2">
+                  <a href="#">Conditions d’utilisation</a>
+                </li>
+                <li className="pb-2">
+                  <a href="#">Charte vendeur</a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex justify-center lg:justify-start">
+              <div className="inline-flex justify-center items-center gap-5 px-5 py-3 bg-light-grey border border-black rounded-full lg:bg-transparent lg:border-none lg:px-0 lg:py-0 lg:pb-2">
+                <a href="#">
+                  <InstagramIcon className="h-6" />
+                </a>
+                <a href="#">
+                  <YoutubeIcon className="h-6" />
+                </a>
+                <a href="#">
+                  <FacebookIcon className="h-6" />
+                </a>
+                <a href="#">
+                  <LinkedInIcon className="h-6" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
+        <div className="mt-10 bg-light-grey">
+          <div className="text-center pt-8 pb-16 lg:container lg:flex lg:justify-between lg:mx-auto lg:px-5 lg:pb-8">
+            <p>SAS HORSETED copyright {currentYear}</p>
+            <p className="hidden lg:block">
+              Site créé par{" "}
+              <a href="https://citronnoir.com/" className="underline">
+                Citron Noir
+              </a>
+            </p>
+          </div>
+        </div>
       </footer>
-      <div className="mt-10 bg-light-grey text-center pt-8 pb-16 lg:flex lg:justify-between lg:px-24 lg:pb-8">
-        <p>SAS HORSETED copyright {currentYear}</p>
-        <p className="hidden lg:block">
-          Site créé par{" "}
-          <a href="https://citronnoir.com/" className="underline">
-            Citron Noir
-          </a>
-        </p>
-      </div>
     </>
   );
 }
