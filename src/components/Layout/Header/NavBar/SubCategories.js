b8ae6eafac66f47d8691e5de53645f9b801e31ec
AdrienLapasset <aflapasset@gmail.com>
2024-04-25 14:24:55 +0200

    add itemcategories to nav

--- src/components/Layout/Header/NavBar/SubCategories.js
+++ src/components/Layout/Header/NavBar/SubCategories.js
@@ -5,16 +5,18 @@ export default async function SubCategories({ className, parentId }) {
   const subCategories = await getCategories(parentId);
 
   return (
-    <ul>
-      {subCategories.map((category) => {
-        const { name, id } = category;
-        return (
-          <li key={name} className="mr-5">
-            {name}
-            <ItemCategories parentId={id} />
-          </li>
-        );
-      })}
-    </ul>
+    <div className="absolute bg-white p-5">
+      <ul>
+        {subCategories.map((category) => {
+          const { name, id } = category;
+          return (
+            <li key={name} className="mr-5">
+              {name}
+              <ItemCategories parentId={id} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
