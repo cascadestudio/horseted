1dff5504ae910648aff411e0d49228562ce6e208
Clément Lapasset <clement.lapasset@gmail.com>
2024-04-26 17:38:37 +0200

    add green homepage section

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -15,8 +15,38 @@ export default function Home() {
   // }, [user]);
 
   return (
-    <main className="font-mcqueen font-thin">
+    <main>
       <RecentProductsSection />
+      <div className="bg-dark-green">
+        <div className="container mx-auto px-5 py-14 flex flex-col gap-14 lg:flex-row lg:gap-44 lg:py-36">
+          <div>
+            <h4 className="font-mcqueen font-bold text-2xl text-white mb-1">
+              Pourquoi choisir des produits d'équitation de seconde main ?
+            </h4>
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur
+            </p>
+          </div>
+          <div>
+            <h4 className="font-mcqueen font-bold text-2xl text-white mb-1">
+              Pourquoi choisir des produits d'équitation de seconde main ?
+            </h4>
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur
+            </p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
