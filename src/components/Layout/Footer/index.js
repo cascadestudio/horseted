d3a661eb254d3b6931e5d02077cbae085e938c95
Clément Lapasset <clement.lapasset@gmail.com>
2024-04-26 10:22:06 +0200

    add max width to app section in footer

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -17,15 +17,22 @@ export default function Footer() {
     <>
       <footer>
         <div className="flex flex-col items-center bg-light-grey px-5 pt-16 lg:pt-32 lg:px-64">
-          <h3 className="font-mcqueen font-bold text-3xl text-center mb-5 lg:text-4xl">
-            Téléchargez l’application Horseted
-          </h3>
-          <p className="font-mcqueen font-medium text-center text-lg mb-10 lg:px-7">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation
-          </p>
-          <Image src={HorsetedApp} alt="App Horseted" />
+          <div className="max-w-[920px]">
+            <h3 className="font-mcqueen font-bold text-3xl text-center mb-5 lg:text-4xl">
+              Téléchargez l’application Horseted
+            </h3>
+            <p className="font-mcqueen font-medium text-center text-lg mb-10 lg:px-7">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation
+            </p>
+            <Image
+              width={920}
+              height={580}
+              src={HorsetedApp}
+              alt="App Horseted"
+            />
+          </div>
         </div>
         <div className="hidden lg:block lg:bg-[#E8E5DB] lg:border-t lg:border-light-green">
           <div className=" lg:container lg:mx-auto lg:flex lg:py-11 lg:justify-between lg:items-center lg:px-5">
