0da2cfd8cdb695e0d4b9440eef4f61543d0d68c2
AdrienLapasset <aflapasset@gmail.com>
2024-04-30 13:43:26 +0200

    display product info

--- src/app/(home)/ProductsSection.js
+++ src/app/(home)/ProductsSection.js
@@ -5,11 +5,11 @@ export default async function RecentProductsSection({ title }) {
   const products = await getApi("products?category=206");
 
   return (
-    <>
-      <h3 className="font-bold text-[32px]">{title}</h3>
-      {products.items.slice(0, 1).map((product) => {
-        return <ProductCard product={product} />;
+    <section className="mt-12">
+      <h3 className="font-bold text-[32px] mb-4">{title}</h3>
+      {products.items.slice(0, 3).map((product) => {
+        return <ProductCard className="w-80" product={product} />;
       })}
-    </>
+    </section>
   );
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,33 @@
 import { getProductImage } from "@/libs/fetch";
 
-export default async function ProductCard({ product }) {
-  const base64 = await getProductImage(
-    `medias/${product.medias[0].files.default}`
-  );
-
+export default async function ProductCard({ product, className }) {
+  console.log(product);
+  const { title, price, favoritCount, size } = product;
   return (
-    <>
-      <h4>{product.title}</h4>
-      <img src={`data:image/png;base64, ${base64}`} alt="Image du produit" />
-    </>
+    <div className={className}>
+      <ProductImage product={product} />
+      <p>{price} €</p>
+      <p>{favoritCount}</p>
+      <h4>{title}</h4>
+      <p>{size?.value}</p>
+    </div>
   );
+}
+
+async function ProductImage({ product }) {
+  if (product.hasOwnProperty("medias")) {
+    const base64 = await getProductImage(
+      `medias/${product.medias[0].files.default}`
+    );
+
+    return (
+      <img
+        className="aspect-[280/340] object-cover"
+        src={`data:image/png;base64, ${base64}`}
+        alt="Image du produit"
+      />
+    );
+  } else {
+    return <p>Pas d'images produit</p>;
+  }
 }
