bfae91fd99ce189d5b764a82e05deb23c2dfd78e
Clément Lapasset <clement.lapasset@gmail.com>
2024-04-30 18:09:34 +0200

    add back blog section

--- src/app/(home)/BlogSection.js
+++ src/app/(home)/BlogSection.js
@@ -1,7 +1,20 @@
 import BlogCard from "@/components/BlogCard";
 import Button from "@/components/Button";
 import RightArrow from "@/assets/icons/RightArrow";
-export default function BlogSection() {
+
+// type Article = {
+//   _id: string
+//   title?: string
+//   slug?: {
+//     current: string
+//   }
+// }
+
+// export async function getStaticProps() {
+//   return await client.fetch<Article[]>(`*[_type == "article"]`)
+// }
+
+export default function BlogSection({ articles }) {
   return (
     <div className="bg-light-grey">
       <div className="container mx-auto px-5 py-14 lg:py-28 lg:flex lg:flex-col lg:items-center">
@@ -11,7 +24,22 @@ export default function BlogSection() {
           Tendances Équestres
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 lg:gap-12">
-          <BlogCard
+          {articles?.length > 0 ? (
+            articles.map((article) => {
+              const { _id, title, image, slug } = article;
+              return (
+                <BlogCard
+                  title={title}
+                  // text={article.body}
+                  image={image}
+                  link={slug.current}
+                />
+              );
+            })
+          ) : (
+            <div className="p-4 text-red-500">No posts found</div>
+          )}
+          {/* <BlogCard
             title="Comment créer de la complicité avec son cheval ?"
             text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut  ..."
             image="https://images.unsplash.com/photo-1502980426475-b83966705988?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&q=80"
@@ -28,7 +56,7 @@ export default function BlogSection() {
             text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut  ..."
             image="https://images.unsplash.com/photo-1502980426475-b83966705988?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=350&q=80"
             link="#"
-          />
+          /> */}
         </div>
         <Button
           className="hidden lg:font-mcqueen lg:font-semibold lg:flex lg:items-center lg:mt-10"
