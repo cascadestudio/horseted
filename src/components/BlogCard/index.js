4a69f485424d10fd4312b863217d85ef7e54e527
Clément Lapasset <clement.lapasset@gmail.com>
2024-05-01 12:59:06 +0200

    add image and rich text to blog section

--- src/components/BlogCard/index.js
+++ src/components/BlogCard/index.js
@@ -1,19 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
 import RightArrow from "@/assets/icons/RightArrow.js";
-export default function BlogCard({ title, text, image, link }) {
+import { urlForImage } from "../../../sanity/lib/image";
+import { PortableText } from "@portabletext/react";
+
+export default function BlogCard({ title, image, body, link }) {
   return (
     <div className="border border-light-green rounded-md max-w-[350px]">
       <div className="flex items-center justify-center w-full p-1">
-        <img
+        <Image
           className="w-full h-52 object-cover rounded-md"
-          src={image}
+          src={urlForImage(image)}
           alt={title}
+          layout="responsive"
+          height={210}
+          width={350}
         />
       </div>
       <div className="p-6">
         <h3 className="font-mcqueen font-semibold text-xl mb-3">{title}</h3>
-        <p className="mb-5">{text}</p>
+        {/* <p className="mb-5">{body}</p> */}
+        <div className="prose prose-stone">
+          <PortableText value={body} />
+        </div>
         <Link className="flex items-center text-light-green" href={link}>
           Lire l'article
           <RightArrow stroke="#4D7A4C" className="ml-2" />
