24890a9f1d11773305a1257640b6f34d0379f2a0
AdrienLapasset <aflapasset@gmail.com>
2024-05-02 18:41:04 +0200

    refactor product page

--- src/app/product/[id]/OfferButton.js
+++ src/app/product/[id]/OfferButton.js
@@ -8,34 +8,15 @@ import Button from "@/components/Button";
 import OfferModal from "./OfferModal";
 import { useState } from "react";
 
-export default function ProductSection({ product }) {
-  const {
-    title,
-    price,
-    userId,
-    description,
-    status,
-    createdAt,
-    shipping,
-    brand,
-    material,
-    favoritCount,
-    color,
-    category,
-    state,
-  } = product;
-
+export default function OfferButton() {
   const [isOfferModal, setIsOfferModal] = useState(false);
 
   return (
-    <section>
-      <h1>{title}</h1>
-      <p>{price} €</p>
-      <Button href="/checkout">Acheter</Button>
+    <>
       <Button onClick={() => setIsOfferModal(true)} variant="transparent">
         Faire une offre
       </Button>
       {isOfferModal && <OfferModal />}
-    </section>
+    </>
   );
 }
