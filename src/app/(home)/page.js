3d90ab2a74ea1a47d66d873a1747beb364bb4680
Clément Lapasset <clement.lapasset@gmail.com>
2024-05-02 11:01:01 +0200

    add mission section layout

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -8,49 +8,55 @@ import missionImage2 from "@/assets/images/missionImage2.jpg";
 
 export default async function Home() {
   const articles = await client.fetch(`*[_type == "article"]`);
-  console.log(articles);
 
   return (
     <main>
       <ProductsSection />
-      <div className="container mx-auto px-5 py-14">
-        <h2 className="font-mcqueen font-bold text-2xl mb-3 lg:mb-4 lg:text-4xl">
-          Notre mission : Développement durable et passion
-        </h2>
-        <p className="lg:text-[18px] mb-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-          <br />
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur
-        </p>
-        <Button variant={"black"} className="w-full justify-center mb-7">
-          En savoir plus
-        </Button>
-        <div className="relative w-full h-[380px] mb-7">
+      <div className="container mx-auto px-5 py-14 lg:flex lg:flex-row-reverse lg:gap-24">
+        <div>
+          <h2 className="font-mcqueen font-bold text-2xl mb-3 lg:mb-4 lg:text-4xl">
+            Notre mission : Agir pour le développement durable et rendre
+            accessible l’équitation à tous les budgets !
+          </h2>
+          <p className="lg:text-[18px] mb-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+            <br />
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur
+          </p>
+          <Button variant={"black"} className="w-full justify-center mb-7">
+            En savoir plus
+          </Button>
           <Image
-            src={missionImage1}
-            fill
+            src={missionImage2}
             sizes="(min-width: 1024px) 590px, 100vw"
-            className="object-cover rounded-lg"
+            className="hidden lg:block object-cover rounded-lg"
           />
         </div>
-        <h2 className="font-mcqueen font-bold text-2xl mb-3 lg:mb-4 lg:text-4xl">
-          Une plateforme conçue pour les acheteurs et les vendeurs
-        </h2>
-        <p className="lg:text-[18px] mb-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. 
-        </p>
-        <Image
-          src={missionImage2}
-          sizes="(min-width: 1024px) 590px, 100vw"
-          className="hidden lg:block object-cover rounded-lg"
-        />
+        <div>
+          <div className="relative w-full h-[380px] mb-7 lg:h-[820px]">
+            <Image
+              src={missionImage1}
+              fill
+              sizes="(min-width: 1024px) 590px, 100vw"
+              className="object-cover rounded-lg"
+            />
+          </div>
+          <div>
+            <h2 className="font-mcqueen font-bold text-2xl mb-3 lg:mb-4 lg:text-4xl">
+              Une plateforme conçue pour les acheteurs et les vendeurs
+            </h2>
+            <p className="lg:text-[18px] mb-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. 
+            </p>
+          </div>
+        </div>
       </div>
       <BlogSection articles={articles} />
       <div className="bg-dark-green">
