c67c4eac0ec56d57c33bb99bd97fcd72566fcdd5
AdrienLapasset <aflapasset@gmail.com>
2024-05-16 11:12:57 +0200

    revert root layout to fix redirect issue

--- src/app/(login)/layout.js
+++ src/app/(login)/layout.js
@@ -1,3 +1,15 @@
+import "../globals.css";
+import { AuthContextProvider } from "@/context/AuthContext";
+import { mcqueen, raleway, baseMetadata } from "@/libs/utils";
+
+export const metadata = baseMetadata;
+
 export default function RootLayout({ children }) {
-  return children;
+  return (
+    <html lang="fr">
+      <body className={`${mcqueen.variable} ${raleway.variable} font-sans`}>
+        <AuthContextProvider>{children}</AuthContextProvider>
+      </body>
+    </html>
+  );
 }
