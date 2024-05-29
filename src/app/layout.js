03091f763cedbfbbc11bf834711671e4333d25b8
Clément Lapasset <clement.lapasset@gmail.com>
2024-05-29 12:08:04 +0200

    fix RootLayout function's name

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { mcqueen, raleway, baseMetadata } from "@/libs/utils";
 
 export const metadata = baseMetadata;
 
-export default function LoginLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className={`${mcqueen.variable} ${raleway.variable} font-sans`}>
