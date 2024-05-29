9df8bfa31e1178aa83ccbc0c63b1d4727c162bcc
Clément Lapasset <clement.lapasset@gmail.com>
2024-05-29 12:58:49 +0200

    clean layouts code

--- src/app/(login)/layout.js
+++ src/app/(login)/layout.js
@@ -1,15 +1,3 @@
-import "../globals.css";
-import { AuthContextProvider } from "@/context/AuthContext";
-import { mcqueen, raleway, baseMetadata } from "@/libs/utils";
-
-export const metadata = baseMetadata;
-
 export default function LoginLayout({ children }) {
-  return (
-    // <html lang="fr">
-    //   <body className={`${mcqueen.variable} ${raleway.variable} font-sans`}>
-    <AuthContextProvider>{children}</AuthContextProvider>
-    //   </body>
-    // </html>
-  );
+  return <>{children}</>;
 }
