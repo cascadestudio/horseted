3f7d4ab11df45d2246fa60e5e7d893b173c654d1
Clément Lapasset <clement.lapasset@gmail.com>
2024-05-30 16:12:04 +0200

    add product images carousel layout

--- src/app/(main)/product/[id]/ProductImagesCarousel.js
+++ src/app/(main)/product/[id]/ProductImagesCarousel.js
@@ -8,31 +8,35 @@ export default function ProductImagesCarousel({ children }) {
   const [nav2, setNav2] = useState(null);
   let sliderRef1 = useRef(null);
   let sliderRef2 = useRef(null);
-
   useEffect(() => {
     setNav1(sliderRef1);
     setNav2(sliderRef2);
   }, []);
   return (
-    <div>
-      <div>
-        <Slider asNavFor={nav2} ref={(slider) => (sliderRef1 = slider)}>
-          {children.map((child, index) => (
-            <div key={index}>
-              <div>{child}</div>
-            </div>
-          ))}
-        </Slider>
+    <div className="flex w-[800px]">
+      <div className="w-3/4">
         <Slider
           asNavFor={nav1}
           ref={(slider) => (sliderRef2 = slider)}
           slidesToShow={3}
           swipeToSlide={true}
           focusOnSelect={true}
+          vertical={true}
+          verticalSwiping={true}
         >
           {children.map((child, index) => (
-            <div key={index}>
-              <div>{child}</div>
+            <div className="w-full h-full" key={index}>
+              {child}
+            </div>
+          ))}
+        </Slider>
+      </div>
+
+      <div className="w-1/4">
+        <Slider asNavFor={nav2} ref={(slider) => (sliderRef1 = slider)}>
+          {children.map((child, index) => (
+            <div className="w-full h-full" key={index}>
+              {child}
             </div>
           ))}
         </Slider>
