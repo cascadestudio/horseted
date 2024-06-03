a4dbd579e5d5fc439f59f46b476d6ed0ccdf6f3a
Clément Lapasset <clement.lapasset@gmail.com>
2024-06-03 13:31:18 +0200

    add poopins font variable

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,15 @@
 import "./globals.css";
 import { AuthContextProvider } from "@/context/AuthContext";
-import { mcqueen, raleway, baseMetadata } from "@/libs/utils";
+import { mcqueen, raleway, poppins, baseMetadata } from "@/libs/utils";
 
 export const metadata = baseMetadata;
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={`${mcqueen.variable} ${raleway.variable} font-sans`}>
+      <body
+        className={`${mcqueen.variable} ${raleway.variable} ${poppins.variable} font-sans`}
+      >
         <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
