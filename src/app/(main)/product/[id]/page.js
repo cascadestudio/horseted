09d808f9618014301244e14d99943ca6d554805a
Clément Lapasset <clement.lapasset@gmail.com>
2024-06-03 18:05:43 +0200

    add products sections in product page

--- src/app/(main)/product/[id]/page.js
+++ src/app/(main)/product/[id]/page.js
@@ -12,6 +12,7 @@ import HeartIcon from "@/assets/icons/HeartIcon";
 import profilePicture from "@/assets/images/profilePicture.jpg";
 import StarIcon from "@/assets/icons/StarIcon.svg";
 import MessageGreenIcon from "@/assets/icons/MessageGreenIcon.svg";
+import ProductsSection from "@/components/ProductsSection";
 
 export default async function ProductPage({ params }) {
   const product = await getApi(`products/${params.id}`);
@@ -37,155 +38,159 @@ export default async function ProductPage({ params }) {
     day: "numeric",
   }).format(new Date(createdAt));
   return (
-    <div className="container mx-auto px-5 flex items-start border-b border-grey mb-11 pb-12">
-      <div className="w-2/3">
-        {product.hasOwnProperty("medias") ? (
-          <ProductImagesCarousel>
-            {medias.map((media) => {
-              return (
-                <div key={media.id}>
-                  <ProductImage media={media} />
-                </div>
-              );
-            })}
-          </ProductImagesCarousel>
-        ) : (
-          <Image
-            className="aspect-[280/340] object-cover w-20"
-            src={placeholderImage}
-            alt="Image du produit"
-          />
-        )}
-      </div>
-      <section className="flex flex-col ml-16">
-        <div className="flex justify-between mb-2">
-          <Link
-            key={category.id}
-            href="#"
-            className="border border-black rounded-3xl py-2 px-3 mt-2 mr-2"
-          >
-            {category.name}
-          </Link>
-          <div className="flex items-center gap-5">
-            <div className="flex items-center gap-1">
-              <HeartIcon />
-              <span>{favoritCount}</span>
-            </div>
-            <Link href="#">
-              <Image src={ShareIcon} alt="ShareIcon" />
-            </Link>
-            <Link href="#">
-              <Image src={ThreeDotsIcon} alt="ThreeDotsIcon" />
-            </Link>
-          </div>
-        </div>
-        <h1 className="font-mcqueen font-bold text-4xl mb-2">{title}</h1>
-        <p className="mb-6">{description}</p>
-        <p className="font-poppins font-semibold text-[28px] leading-[42px]">
-          {price} €
-        </p>
-        <p className="font-poppins text-light-green text-sm mb-2">
-          5,90€ <span className="font-sans">- Livraison à domicile</span>
-        </p>
-        <Button href="/checkout" className="w-full flex justify-center mb-3">
-          Acheter
-        </Button>
-        <OfferButton />
-        <div className="flex justify-between items-center mt-3 border border-light-green rounded-2xl pl-6 pt-4 pr-3 pb-6">
-          <div>
-            <h4 className="font-mcqueen font-bold text-lg leading-5">
-              Acheter un lot
-            </h4>
-            <p className="text-sm">Économisez sur les frais de livraison</p>
-          </div>
-          <Button href="#" className="text-sm whitespace-nowrap h-8">
-            Créer un lot
-          </Button>
-        </div>
-        <table className="table-auto mt-5">
-          <tbody className="[&>tr]:flex [&>tr]:justify-between [&>tr]:border-b [&>tr]:border-grey [&>tr]:py-2 [&_td] [&_td]:font-semibold [&_td]:text-sm [&_td]:leading-6 [&_a]:text-light-green [&_a]:underline">
-            <tr>
-              <td>Catégorie</td>
-              <td>
-                <Link href="#">{category.name}</Link>
-              </td>
-            </tr>
-            <tr>
-              <td>État</td>
-              <td>
-                <Link href="#">{state}</Link>
-              </td>
-            </tr>
-            <tr>
-              <td>Couleurs</td>
-              <td>
-                <Link href="#">{color.name}</Link>
-              </td>
-            </tr>
-            <tr>
-              <td>Marque</td>
-              <td>
-                <Link href="#">{brand}</Link>
-              </td>
-            </tr>
-            <tr>
-              <td>Matières</td>
-              <td>
-                <Link href="#">{material}</Link>
-              </td>
-            </tr>
-            <tr>
-              <td>Taille</td>
-              <td>
-                <Link href="#">16 ans</Link>
-              </td>
-            </tr>
-            <tr>
-              <td>Taille du colis</td>
-              <td>
-                <Link href="#">{shipping}</Link>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div className="flex justify-between w-full mt-3 mb-5">
-          <div className="flex">
+    <div>
+      <div className="container mx-auto px-5 flex items-start border-b border-grey mb-11 pb-12">
+        <div className="w-2/3">
+          {product.hasOwnProperty("medias") ? (
+            <ProductImagesCarousel>
+              {medias.map((media) => {
+                return (
+                  <div key={media.id}>
+                    <ProductImage media={media} />
+                  </div>
+                );
+              })}
+            </ProductImagesCarousel>
+          ) : (
             <Image
-              src={profilePicture}
-              alt="Photo de profil"
-              className="h-14 w-14 object-cover rounded-full"
+              className="aspect-[280/340] object-cover w-20"
+              src={placeholderImage}
+              alt="Image du produit"
             />
-            <div>
-              <h4 className="font-mcqueen font-bold text-lg leading-5 ml-3">
-                Alexandra-ast
-              </h4>
-              <div className="flex items-center ml-3">
-                <Image src={StarIcon} alt="StarIcon" />
-                <Image src={StarIcon} alt="StarIcon" />
-                <Image src={StarIcon} alt="StarIcon" />
-                <Image src={StarIcon} alt="StarIcon" />
-                <Image src={StarIcon} alt="StarIcon" />
-                <span className="ml-2">(6)</span>
+          )}
+        </div>
+        <section className="flex flex-col ml-16">
+          <div className="flex justify-between mb-2">
+            <Link
+              key={category.id}
+              href="#"
+              className="border border-black rounded-3xl py-2 px-3 mt-2 mr-2"
+            >
+              {category.name}
+            </Link>
+            <div className="flex items-center gap-5">
+              <div className="flex items-center gap-1">
+                <HeartIcon />
+                <span>{favoritCount}</span>
               </div>
+              <Link href="#">
+                <Image src={ShareIcon} alt="ShareIcon" />
+              </Link>
+              <Link href="#">
+                <Image src={ThreeDotsIcon} alt="ThreeDotsIcon" />
+              </Link>
             </div>
           </div>
-          <div className="flex items-center">
-            <Link
-              href="#"
-              className="h-8 w-8 pr-4 box-content mr-4 border-r border-grey flex justify-center items-center "
-            >
-              <Image src={MessageGreenIcon} alt="Écrire un message" />
-            </Link>
-            <Button
-              href="#"
-              className="text-sm whitespace-nowrap h-8 px-4 py-2 flex justify-center max-w-[110px]"
-            >
-              Voir le profil
+          <h1 className="font-mcqueen font-bold text-4xl mb-2">{title}</h1>
+          <p className="mb-6">{description}</p>
+          <p className="font-poppins font-semibold text-[28px] leading-[42px]">
+            {price} €
+          </p>
+          <p className="font-poppins text-light-green text-sm mb-2">
+            5,90€ <span className="font-sans">- Livraison à domicile</span>
+          </p>
+          <Button href="/checkout" className="w-full flex justify-center mb-3">
+            Acheter
+          </Button>
+          <OfferButton />
+          <div className="flex justify-between items-center mt-3 border border-light-green rounded-2xl pl-6 pt-4 pr-3 pb-6">
+            <div>
+              <h4 className="font-mcqueen font-bold text-lg leading-5">
+                Acheter un lot
+              </h4>
+              <p className="text-sm">Économisez sur les frais de livraison</p>
+            </div>
+            <Button href="#" className="text-sm whitespace-nowrap h-8">
+              Créer un lot
             </Button>
           </div>
-        </div>
-        <p className="self-end">Ajouté le {formattedDate}</p>
-      </section>
+          <table className="table-auto mt-5">
+            <tbody className="[&>tr]:flex [&>tr]:justify-between [&>tr]:border-b [&>tr]:border-grey [&>tr]:py-2 [&_td] [&_td]:font-semibold [&_td]:text-sm [&_td]:leading-6 [&_a]:text-light-green [&_a]:underline">
+              <tr>
+                <td>Catégorie</td>
+                <td>
+                  <Link href="#">{category.name}</Link>
+                </td>
+              </tr>
+              <tr>
+                <td>État</td>
+                <td>
+                  <Link href="#">{state}</Link>
+                </td>
+              </tr>
+              <tr>
+                <td>Couleurs</td>
+                <td>
+                  <Link href="#">{color.name}</Link>
+                </td>
+              </tr>
+              <tr>
+                <td>Marque</td>
+                <td>
+                  <Link href="#">{brand}</Link>
+                </td>
+              </tr>
+              <tr>
+                <td>Matières</td>
+                <td>
+                  <Link href="#">{material}</Link>
+                </td>
+              </tr>
+              <tr>
+                <td>Taille</td>
+                <td>
+                  <Link href="#">16 ans</Link>
+                </td>
+              </tr>
+              <tr>
+                <td>Taille du colis</td>
+                <td>
+                  <Link href="#">{shipping}</Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="flex justify-between w-full mt-3 mb-5">
+            <div className="flex">
+              <Image
+                src={profilePicture}
+                alt="Photo de profil"
+                className="h-14 w-14 object-cover rounded-full"
+              />
+              <div>
+                <h4 className="font-mcqueen font-bold text-lg leading-5 ml-3">
+                  Alexandra-ast
+                </h4>
+                <div className="flex items-center ml-3">
+                  <Image src={StarIcon} alt="StarIcon" />
+                  <Image src={StarIcon} alt="StarIcon" />
+                  <Image src={StarIcon} alt="StarIcon" />
+                  <Image src={StarIcon} alt="StarIcon" />
+                  <Image src={StarIcon} alt="StarIcon" />
+                  <span className="ml-2">(6)</span>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <Link
+                href="#"
+                className="h-8 w-8 pr-4 box-content mr-4 border-r border-grey flex justify-center items-center "
+              >
+                <Image src={MessageGreenIcon} alt="Écrire un message" />
+              </Link>
+              <Button
+                href="#"
+                className="text-sm whitespace-nowrap h-8 px-4 py-2 flex justify-center max-w-[110px]"
+              >
+                Voir le profil
+              </Button>
+            </div>
+          </div>
+          <p className="self-end">Ajouté le {formattedDate}</p>
+        </section>
+      </div>
+      <ProductsSection title="Sellerie de" />
+      <ProductsSection title="Articles similaires" />
     </div>
   );
 }
