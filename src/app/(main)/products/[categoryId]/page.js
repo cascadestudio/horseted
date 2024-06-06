33e8033c0cf9d89f103f7af6d81e95717ea1035c
AdrienLapasset <aflapasset@gmail.com>
2024-06-06 14:23:20 +0200

    use client in products page

--- src/app/(main)/products/[categoryId]/page.js
+++ src/app/(main)/products/[categoryId]/page.js
@@ -1,18 +1,56 @@
+"use client";
 import ProductCard from "@/components/ProductCard";
-import { getApi } from "@/libs/fetch";
+// import { getApi } from "@/libs/fetch";
+import { useState, useEffect } from "react";
 
-export default async function ProductsPage({ params }) {
-  const products = await getApi(`products?category=${params.categoryId}`);
+export default function ProductsPage({ params }) {
+  // const products = await getApi(`products?category=${params.categoryId}`);
 
-  // console.log(products);
+  const [sortOption, setSortOption] = useState("");
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_HORSETED_API_BASE_URL}/products?category=${params.categoryId}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "API-Key": process.env.NEXT_PUBLIC_HORSETED_API_KEY,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+        setIsLoading(false);
+      });
+  }, [sortOption]);
 
   return (
     <>
       <h1>{params.categoryId}</h1>
+      <div className="p-5">
+        <label htmlFor="sort">Trier par</label>
+        <select name="sort" id="sort">
+          <option value="">Nouveautés</option>
+          <option value="">Populaires</option>
+          <option value="">Prix croissant</option>
+          <option value="">Prix décroissant</option>
+        </select>
+      </div>
 
-      {products.items.map((product) => {
-        return <ProductCard className="mr-5" product={product} />;
-      })}
+      {!isLoading &&
+        products.items.map((product) => {
+          return (
+            <div key={product.id}>
+              {product.title}
+              {/* TODO product card in client component */}
+              {/* <ProductCard className="mr-5" product={product} />; */}
+            </div>
+          );
+        })}
     </>
   );
 }
