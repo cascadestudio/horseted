66451fc4a479d6f6d1661f83824ad5951a8fca11
AdrienLapasset <aflapasset@gmail.com>
2024-06-26 15:48:19 +0200

    redirect certain pages if user is not logged in

--- src/app/(main)/account/page.js
+++ src/app/(main)/account/page.js
@@ -1,14 +1,24 @@
 "use client";
 import { useAuthContext } from "@/context/AuthContext";
 import Button from "@/components/Button";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function AccountPage() {
   const { user } = useAuthContext();
-  return (
-    <>
-      {user.username}
-      <br />
-      {user.description}
-      <Button href="/settings">Modifier mon profil</Button>
-    </>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    if (user === null) {
+      router.push("/");
+    }
+  }, [user]);
+
+  if (user)
+    return (
+      <>
+        {user?.username}
+        <br />
+        {user?.description}
+        <Button href="/settings">Modifier mon profil</Button>
+      </>
+    );
 }
