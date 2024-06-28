dc50b4b7efa970d5f8f9e78102c9fefd2f1868ae
AdrienLapasset <aflapasset@gmail.com>
2024-06-28 11:11:31 +0200

    add dynamic title and links

--- src/app/(main)/parametres/layout.js
+++ src/app/(main)/parametres/layout.js
@@ -1,21 +1,48 @@
 "use client";
 import withAuth from "@/hoc/withAuth";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
+const pages = [
+  { title: "Paramètres du profil", path: "" },
+  { title: "Personnalisation", path: "personalisation" },
+  {
+    title: "Paiements & encaissements",
+    path: "paiements-encaissements",
+  },
+  { title: "Adresses", path: "adresses" },
+  { title: "Notifications", path: "notifications" },
+  { title: "Mode vacances", path: "mode-vacances" },
+];
 
 function SettingsLayout({ children }) {
+  const pathname = usePathname();
+  const [activePath, setActivePath] = useState("");
+
+  useEffect(() => {
+    const path = pathname.substring(pathname.lastIndexOf("/") + 1);
+    if (path === "parametres") {
+      setActivePath("");
+    } else {
+      setActivePath(path || "");
+    }
+  }, [pathname]);
+
   return (
-    <div>
-      <aside>
-        <h1>Paramètres</h1>
-        <div>
-          <a href="/parametres">Paramètres du profil</a>
-          <a href="/parametres/personalisation">Personnalisation</a>
-          <a>Paiements & encaissements</a>
-          <a>Adresses</a>
-          <a>Notifications</a>
-          <a>Mode vacances</a>
-        </div>
-      </aside>
-      {children}
+    <div className="grid grid-cols-2">
+      <div>
+        <h1 className="font-mcqueen font-bold text-2xl mb-5">
+          {pages.find((page) => page.path === activePath)?.title}
+        </h1>
+        <aside className="bg-light-grey flex flex-col">
+          {pages.map((page, index) => (
+            <a className="p-4" key={index} href={`/parametres/${page.path}`}>
+              {page.title}
+            </a>
+          ))}
+        </aside>
+      </div>
+      <div>{children}</div>
     </div>
   );
 }
