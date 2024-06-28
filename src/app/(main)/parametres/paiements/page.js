39c18f80a91e5dc4198e9cbd250a04f044d4d649
AdrienLapasset <aflapasset@gmail.com>
2024-06-28 12:37:39 +0200

    add payment card modal state

--- src/app/(main)/parametres/paiements/page.js
+++ src/app/(main)/parametres/paiements/page.js
@@ -1,10 +1,19 @@
+"use client";
+import { useState } from "react";
+import AddPaymentCardModal from "./AddPaymentCardModal";
+
 export default function PaymentSettings() {
+  const [isModal, setIsModal] = useState(false);
   return (
     <div>
       <h2 className="font-mcqueen font-bold text-xl mb-5">Mode de paiement</h2>
-      <button className="flex items-center justify-center p-2 bg-light-green rounded-full hover:bg-dark-green">
+      <button
+        onClick={() => setIsModal(true)}
+        className="flex items-center justify-center p-2 bg-light-green rounded-full hover:bg-dark-green"
+      >
         + Ajouter une carte bancaire
       </button>
+      {isModal && <AddPaymentCardModal />}
     </div>
   );
 }
