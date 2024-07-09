4114d8f1c680d76cf74fa02337e4c91f3050845f
Clément Lapasset <clement.lapasset@gmail.com>
2024-07-09 18:11:10 +0200

    add dynamic color to svg

--- src/assets/icons/RightArrow.js
+++ src/assets/icons/RightArrow.js
@@ -1,23 +1,23 @@
-export default function RightArrow({ className, color }) {
+export default function RightArrow({ className }) {
   return (
     <svg
-      className={className}
+      className={`${className} fill-current`}
       width="13"
       height="10"
-      viewBox="0 0 13 10"
       fill="none"
+      viewBox="0 0 13 10"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M1.5 5H11.5"
-        stroke={color}
+        stroke="currentColor"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M7.50024 8.99997L11.5002 4.99998L7.50024 1"
-        stroke={color}
+        stroke="currentColor"
         strokeWidth="1.5"
         strokeLinecap="round"
       />
