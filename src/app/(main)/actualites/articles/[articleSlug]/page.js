7cbcc714adff0b1aba0ad8f0c9b194feba21415c
Clément Lapasset <clement.lapasset@gmail.com>
2024-07-18 12:47:16 +0200

    display similar articles in article page

--- src/app/(main)/actualites/articles/[articleSlug]/page.js
+++ src/app/(main)/actualites/articles/[articleSlug]/page.js
@@ -22,16 +22,23 @@ async function getArticleData(slug) {
     { slug }
   );
 
-  if (!articles.length) {
-    return { notFound: true };
-  }
+  const categoryArticles = await client.fetch(
+    `*[_type == "article" && references($categoryId)]{
+      title,
+      body,
+      image,
+      slug,
+      "category": category->{title, _id, slug}
+    }`,
+    { categoryId: articles[0].category._id }
+  );
 
-  return articles[0];
+  return { article: articles[0], categoryArticles };
 }
 
 export default async function ArticlePage({ params }) {
   const { articleSlug } = params;
-  const article = await getArticleData(articleSlug);
+  const { article, categoryArticles } = await getArticleData(articleSlug);
 
   if (article.notFound) {
     return <div>Article not found</div>;
@@ -47,23 +54,23 @@ export default async function ArticlePage({ params }) {
           <h1 className="font-mcqueen font-bold text-3xl text-center mb-5 lg:text-4xl max-w-[610px]">
             {title}
           </h1>
-          <div className="grid grid-cols-[1fr,auto,1fr] items-center gap-7">
-            <BurstIcon className="transform scale-x-[-1] self-start h-10 w-10 mt-9 place-self-end" />
-            <div className="relative">
+          <div className="grid grid-cols-2 lg:grid-cols-[1fr,auto,1fr] items-center gap-7">
+            <BurstIcon className="transform scale-x-[-1] h-10 w-10 lg:mt-9 lg:self-start  lg:place-self-end " />
+            <div className="relative col-span-3 lg:col-span-1">
               {image && (
                 <Image
                   src={urlForImage(image)}
                   alt={title}
                   width={610}
                   height={320}
-                  className="object-cover h-[320px] w-[610px] rounded-[32px] border border-white"
+                  className="object-cover lg:h-[320px] lg:w-[610px] rounded-[32px] border border-white"
                 />
               )}
               <span className="absolute bottom-2 right-2 bg-light-green rounded-[20px] border border-white px-4 py-2">
                 {category.title}
               </span>
             </div>
-            <div className="pb-16">
+            <div className="row-start-1 row-span-1 col-start-2 lg:col-start-3 lg:pb-16 ">
               <span className="font-extrabold">Partager :</span>
               <div className="flex items-center space-x-3 mt-2">
                 <FacebookIconNoBorder className="h-4" />
@@ -80,16 +87,11 @@ export default async function ArticlePage({ params }) {
         <div className="prose max-w-none mb-10 text-black">
           <PortableText value={body} components={PortableTextComponents} />
         </div>
-        <div className="mb-10">
-          <div className="flex flex-wrap gap-2">
-            <div className="mb-10">
-              <h3 className="font-mcqueen font-semibold text-lg mb-3">
-                Sur le même sujet
-              </h3>
-              {/* <CategoryBlogSection category={category}  /> */}
-            </div>
-          </div>
-        </div>
+
+        <h3 className="font-mcqueen font-semibold text-lg mb-3">
+          Sur le même sujet
+        </h3>
+        <CategoryBlogSection category={category} articles={categoryArticles} />
       </div>
     </div>
   );
