25f9f815f631684950ba7d329500aac8a9acc882
Clément Lapasset <clement.lapasset@gmail.com>
2024-07-22 15:17:13 +0200

    clean code

--- src/app/(main)/parametres/layout.js
+++ src/app/(main)/parametres/layout.js
@@ -40,10 +40,7 @@ function SettingsLayout({ children }) {
   return (
     <div className="container mx-auto px-5 grid grid-cols-3 gap-14 pt-11 pb-16">
       <div className="col-span-3 lg:col-span-1">
-        <h1 className="font-mcqueen font-bold text-4xl mb-5">
-          {/* {pages.find((page) => page.path === activePath)?.title} */}
-          Paramètres
-        </h1>
+        <h1 className="font-mcqueen font-bold text-4xl mb-5">Paramètres</h1>
         <aside className="bg-light-grey flex flex-col border border-lighter-grey">
           {pages.map((page, index) => {
             const Icon = page.icon;
