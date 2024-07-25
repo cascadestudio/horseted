4a69ee5f88ab3de7dd526d0f8fc89a693f7c61fa
AdrienLapasset <aflapasset@gmail.com>
2024-07-25 13:13:21 +0200

    filter dropdown style

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,17 +1,23 @@
 import { useState } from "react";
 
-export default function Dropdown({ title, children, className }) {
-  const [isActive, setIsActive] = useState(false);
+export default function Dropdown({ title, children, className, isActive }) {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={className}>
       <button
-        onClick={() => setIsActive(!isActive)}
-        className="flex items-center border border-light-green px-5 py-2 rounded-xl bg-lighter-green text-light-green font-mcqueen font-semibold"
+        onClick={() => setIsOpen(!isOpen)}
+        className={`flex items-center border px-5 py-2 rounded-xl font-mcqueen font-semibold ${
+          isActive
+            ? "border-light-green text-light-green bg-lighter-green"
+            : "text-medium-grey border-medium-grey"
+        }`}
       >
         {title}
         <svg
-          className="ml-2"
+          className={`ml-2 ${
+            isActive ? " stroke-light-green" : "stroke-medium-grey"
+          }`}
           width="10"
           height="7"
           viewBox="0 0 10 7"
@@ -20,14 +26,13 @@ export default function Dropdown({ title, children, className }) {
         >
           <path
             d="M1.00003 1.5L5.00002 5.49998L9 1.5"
-            stroke="#4D7A4C"
             strokeWidth="1.5"
             strokeLinecap="round"
             strokeLinejoin="round"
           />
         </svg>
       </button>
-      {isActive && (
+      {isOpen && (
         <div className="absolute bg-light-grey border border-light-green rounded-xl p-5 mt-2 z-10">
           {children}
         </div>
