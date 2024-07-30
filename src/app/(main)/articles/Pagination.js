9d359f254b3ccaa6ae587c57c58ad865246477dd
AdrienLapasset <aflapasset@gmail.com>
2024-07-30 12:49:19 +0200

    handle pagination

--- src/app/(main)/articles/Pagination.js
+++ src/app/(main)/articles/Pagination.js
@@ -1,61 +1,67 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Pagination({ products, setFromId }) {
-  console.log("first products", products.items[0].id);
+  const [currentPageIndex, setCurrentPageIndex] = useState(0);
+  console.log("currentPageIndex", currentPageIndex);
+  const [pages, setPages] = useState([
+    {
+      firstProductId: products.items[0].id,
+      lastProductId: products.items[products.items.length - 1].id,
+    },
+  ]);
+  console.log("pages", pages);
 
-  const totalProduct = products.total;
-  const itemsPerPage = 50;
-  const totalPages = Math.ceil(totalProduct / itemsPerPage);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [prevPageFirstProductId, setPrevPageFirstProductId] = useState();
-
-  const getVisiblePages = () => {
-    let startPage = Math.max(1, currentPage - 1);
-    let endPage = Math.min(totalPages, currentPage + 1);
-
-    if (currentPage === 1) {
-      endPage = Math.min(totalPages, 3);
-    } else if (currentPage === totalPages) {
-      startPage = Math.max(1, totalPages - 2);
+  useEffect(() => {
+    if (!pages[currentPageIndex]) {
+      setPages((prev) => [
+        ...prev,
+        {
+          firstProductId: products.items[0].id,
+          lastProductId: products.items[products.items.length - 1].id,
+        },
+      ]);
     }
+  }, [products]);
 
-    const pages = [];
-    for (let i = startPage; i <= endPage; i++) {
-      pages.push(i);
+  const handleClick = (pageIndex) => {
+    setCurrentPageIndex(pageIndex);
+    if (pageIndex > currentPageIndex) {
+      setFromId(pages[pageIndex - 1].lastProductId);
+    } else {
+      const firstProductIdFromPageIndex = pages[pageIndex].firstProductId;
+      setFromId(firstProductIdFromPageIndex);
     }
-
-    return pages;
   };
 
-  const visiblePages = getVisiblePages();
-
-  const handleClick = (page) => {
-    const lastProductId = products.items[products.items.length - 1].id;
-    setPrevPageFirstProductId(products.items[0].id);
-    if (page === currentPage - 1) {
-      setFromId(prevPageFirstProductId);
-    }
-    if (page === currentPage + 1) {
-      setFromId(lastProductId);
-    }
-
-    setCurrentPage(page);
-  };
+  const isNextButton = products.total > pages.length * 50;
 
   return (
     <nav className="py-10">
       <ul className=" flex gap-x-4  justify-center ">
-        {visiblePages.map((page) => (
-          <li key={page}>
-            <button
-              onClick={() => handleClick(page)}
-              className={currentPage === page ? "text-dark-green" : ""}
-            >
-              {page}
-            </button>
-          </li>
-        ))}
+        {pages.length > 0 &&
+          pages.map((page, index) => {
+            return (
+              <>
+                <button
+                  key={index}
+                  onClick={() => handleClick(index)}
+                  className={`px-4 py-2 border rounded ${
+                    index === currentPageIndex ? "bg-gray-200" : ""
+                  }`}
+                >
+                  {index + 1}
+                </button>
+              </>
+            );
+          })}
+        {isNextButton && (
+          <button
+            onClick={() => handleClick(pages.length)}
+            className={`px-4 py-2 border rounded`}
+          >
+            {pages.length + 1}
+          </button>
+        )}
       </ul>
     </nav>
   );
--- src/components/HeroCarousel/index.js
+++ src/components/HeroCarousel/index.js
@@ -62,6 +62,7 @@ export default function HeroCarousel() {
       <Slider {...settings}>
         <div className="relative">
           <Image
+            quality="90"
             src={heroImage1}
             alt="hero image 1"
             priority
