dc6cf6d821646be39a09e983e48b1c9af6a3be40
AdrienLapasset <aflapasset@gmail.com>
2024-08-08 14:17:16 +0200

    style

--- src/app/(main)/articles/ProductList.js
+++ src/app/(main)/articles/ProductList.js
@@ -2,15 +2,14 @@ import ProductCard from "@/components/ProductCard";
 
 export default function ProductList({ products }) {
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-14">
+    <section className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-14">
       {products.items.map((product) => {
         return (
           <div key={product.id} className="">
             <ProductCard product={product} />
-            {/* TODO pagination */}
           </div>
         );
       })}
-    </div>
+    </section>
   );
 }
