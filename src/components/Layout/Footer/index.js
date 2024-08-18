e9497ffa60f883facb2ae2b41229023552f2c67b
Clément Lapasset <clement.lapasset@gmail.com>
2024-08-19 00:57:41 +0200

    add alt to logo

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -79,6 +79,7 @@ export default function Footer() {
           <div className="flex items-center flex-col mb-8">
             <Image
               src={HorsetedLogoBlackIcon}
+              alt="Horseted Logo"
               className="h-24 w-24 self-center mb-3 lg:h-40 lg:w-40 lg:mb-7"
             />
             <Button href="/vendre" className="px-16 w-full lg:w-auto">
