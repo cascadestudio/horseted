a89ac13a241b4a520b2c833ba806b2999d980239
AdrienLapasset <aflapasset@gmail.com>
2024-08-19 12:41:59 +0200

    display seller account

--- src/app/(main)/parametres/encaissements/DisplaySellerAccount.js
+++ src/app/(main)/parametres/encaissements/DisplaySellerAccount.js
@@ -1,16 +1,39 @@
 import React from "react";
 
-export default function DisplaySellerAccount({ sellerData, user }) {
+export default function DisplaySellerAccount({ sellerData }) {
+  const {
+    ibanLast4,
+    address,
+    dateOfBirth,
+    firstName,
+    lastName,
+    verificationStatus,
+  } = sellerData;
   console.log("sellerData =>", sellerData);
-  console.log("user =>", user);
   return (
     <div className="grid grid-cols-1 lg:pt-5 lg:grid-cols-2 lg:gap-x-14 gap-y-4">
-      <div className="col-span-2 lg:col-span-1">
+      <div className="col-span-2 lg:col-span-1 flex flex-col">
         <h2 className="font-mcqueen text-[24px] font-bold">
           Informations vendeur
         </h2>
-        IBAN : **** **** **** {sellerData.ibanLast4}
-        Adresse d’expédition* :
+        <div>IBAN : **** **** **** {ibanLast4}</div>
+        <div>
+          Adresse d’expédition* :{address.city}
+          {address.postalCode}
+          <br />
+          {address.street}
+        </div>
+      </div>
+      <div className="col-span-2 lg:col-span-1">
+        <h2 className="font-mcqueen text-[24px] font-bold">
+          Vérification de l’identité
+        </h2>
+        {verificationStatus === "unverified" && (
+          <p className="text-light-yellow">En cours</p>
+        )}
+        {firstName} <br />
+        {lastName} <br />
+        {dateOfBirth} <br />
       </div>
     </div>
   );
