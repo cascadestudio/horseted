1492df8bb6878a0e0be45871c1d962fd00111475
AdrienLapasset <aflapasset@gmail.com>
2024-09-03 18:36:36 +0200

    add post product summary page

---
+++ src/app/(main)/vendre/ProductSummary.js
@@ -0,0 +1,44 @@
+import getImage from "@/utils/getImage";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function ProductSummary({ postResponse }) {
+  const [isImageLoading, setIsImageLoading] = useState(false);
+  const [imageSrcs, setImageSrcs] = useState([]);
+  const { title, brand, size, medias } = postResponse;
+
+  useEffect(() => {
+    if (medias?.length) {
+      medias.forEach(async (media) => {
+        setIsImageLoading(true);
+        await getMedia(media.files.thumbnail200);
+        setIsImageLoading(false);
+      });
+    }
+  }, [medias]);
+
+  async function getMedia(file) {
+    const src = await getImage(file, "client");
+    setImageSrcs((prev) => [...prev, src]);
+  }
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{brand}</p>
+      <p>{size.value}</p>
+
+      {imageSrcs.length > 0 &&
+        imageSrcs.map((imageSrc, index) => (
+          <Image
+            key={index}
+            src={imageSrc}
+            className={`object-cover rounded`}
+            width={100}
+            height={100}
+            alt="Avatar"
+          />
+        ))}
+    </div>
+  );
+}
--- src/app/(main)/vendre/page.js
+++ src/app/(main)/vendre/page.js
@@ -16,10 +16,12 @@ import Colors from "./selects/Colors";
 import Brand from "./selects/Brand";
 import Materials from "./selects/Materials";
 import Shipping from "./selects/Shipping";
+import ProductSummary from "./ProductSummary";
 
 export default function SellPage() {
   const { accessToken } = useAuthContext();
   const [isLoading, setIsLoading] = useState(false);
+  const [postResponse, setPostResponse] = useState(null);
   const [product, setProduct] = useState({
     title: "",
     price: "",
@@ -51,8 +53,9 @@ export default function SellPage() {
       true,
       true
     );
-    setIsLoading(false);
     console.log("response =>", response);
+    setIsLoading(false);
+    setPostResponse(response);
   };
 
   if (isLoading) return <Spinner />;
@@ -69,75 +72,81 @@ export default function SellPage() {
           </div>
         </div>
       </div>
-      <form
-        onSubmit={handleFormSubmit}
-        className="container mx-auto px-5 pt-5 flex flex-col items-center gap-7"
-      >
-        <ProductMedia accessToken={accessToken} setProduct={setProduct} />
-        <div className="w-full flex justify-center">
-          <h3 className="font-mcqueen font-semibold w-[200px]">
-            Titre de l'article* :
-          </h3>
-          <TextInput
-            onChange={handleFormChange}
-            name="title"
-            value={product.title}
-            required
-            className="max-w-[700px]"
-            hideLabel
-            placeholder="Ex : Couverture de poney"
-          />
-        </div>
-        <div className="w-full flex justify-center">
-          <h3 className="font-mcqueen font-semibold w-[200px]">
-            Description de l'article :
-          </h3>
-          <TextInput
-            onChange={handleFormChange}
-            name="description"
-            value={product.description}
-            required
-            className="max-w-[700px]"
-            hideLabel
-            type="textarea"
-            placeholder="Ex : Acheté le 10/12/2024, porté quelques fois mais ne me convient pas. Très bon état...poney"
-          />
-        </div>
-        <div className="w-full flex justify-center">
-          <h3 className="font-mcqueen font-semibold w-[200px]">Prix* :</h3>
-          <label
-            className="font-mcqueen font-semibold w-full max-w-[700px] "
-            htmlFor="price"
-          >
-            <div className="flex items-center border-b border-black">
-              <input
-                onChange={handleFormChange}
-                name="price"
-                value={product.price}
-                required
-                type="number"
-                step="0.01"
-                placeholder="Ex : 20"
-                className="focus:outline-none border-none bg-transparent w-full placeholder:text-grey pt-1 pb-2 resize-none overflow-hidden break-words whitespace-pre-wrap"
-              />
-              <span className="text-xl font-mcqueen font-semibold mr-2">€</span>
-            </div>
-          </label>
-        </div>
-        <Category product={product} setProduct={setProduct} />
-        <State product={product} setProduct={setProduct} />
-        <Size product={product} setProduct={setProduct} />
-        <Colors product={product} setProduct={setProduct} />
-        <Brand product={product} setProduct={setProduct} />
-        <Materials product={product} setProduct={setProduct} />
-        <Shipping product={product} setProduct={setProduct} />
-        <Button
-          onClick={handleFormSubmit}
-          className="w-full max-w-[900px] mb-5"
+      {postResponse ? (
+        <ProductSummary postResponse={postResponse} />
+      ) : (
+        <form
+          onSubmit={handleFormSubmit}
+          className="container mx-auto px-5 pt-5 flex flex-col items-center gap-7"
         >
-          Publier l'article
-        </Button>
-      </form>
+          <ProductMedia accessToken={accessToken} setProduct={setProduct} />
+          <div className="w-full flex justify-center">
+            <h3 className="font-mcqueen font-semibold w-[200px]">
+              Titre de l'article* :
+            </h3>
+            <TextInput
+              onChange={handleFormChange}
+              name="title"
+              value={product.title}
+              required
+              className="max-w-[700px]"
+              hideLabel
+              placeholder="Ex : Couverture de poney"
+            />
+          </div>
+          <div className="w-full flex justify-center">
+            <h3 className="font-mcqueen font-semibold w-[200px]">
+              Description de l'article :
+            </h3>
+            <TextInput
+              onChange={handleFormChange}
+              name="description"
+              value={product.description}
+              required
+              className="max-w-[700px]"
+              hideLabel
+              type="textarea"
+              placeholder="Ex : Acheté le 10/12/2024, porté quelques fois mais ne me convient pas. Très bon état...poney"
+            />
+          </div>
+          <div className="w-full flex justify-center">
+            <h3 className="font-mcqueen font-semibold w-[200px]">Prix* :</h3>
+            <label
+              className="font-mcqueen font-semibold w-full max-w-[700px] "
+              htmlFor="price"
+            >
+              <div className="flex items-center border-b border-black">
+                <input
+                  onChange={handleFormChange}
+                  name="price"
+                  value={product.price}
+                  required
+                  type="number"
+                  step="0.01"
+                  placeholder="Ex : 20"
+                  className="focus:outline-none border-none bg-transparent w-full placeholder:text-grey pt-1 pb-2 resize-none overflow-hidden break-words whitespace-pre-wrap"
+                />
+                <span className="text-xl font-mcqueen font-semibold mr-2">
+                  €
+                </span>
+              </div>
+            </label>
+          </div>
+          <Category product={product} setProduct={setProduct} />
+          <State product={product} setProduct={setProduct} />
+          <Size product={product} setProduct={setProduct} />
+          <Colors product={product} setProduct={setProduct} />
+          <Brand product={product} setProduct={setProduct} />
+          <Materials product={product} setProduct={setProduct} />
+          <Shipping product={product} setProduct={setProduct} />
+          <Button
+            onClick={handleFormSubmit}
+            className="w-full max-w-[900px] mb-5"
+          >
+            Publier l'article
+          </Button>
+        </form>
+      )}
     </div>
   );
 }
