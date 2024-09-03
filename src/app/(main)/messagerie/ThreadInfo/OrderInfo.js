c1bed998e4268f6761049649f8a4e54b7742f6d7
AdrienLapasset <aflapasset@gmail.com>
2024-09-03 12:38:18 +0200

    fix

--- src/app/(main)/messagerie/ThreadInfo/OrderInfo.js
+++ src/app/(main)/messagerie/ThreadInfo/OrderInfo.js
@@ -8,7 +8,7 @@ export default function OrderInfo({
   accessToken,
   orderId,
 }) {
-  // console.log("orderTracking =>", orderTracking);
+  console.log("orderTracking =>", orderTracking);
 
   const handleIsReceived = async () => {
     const query = `/orders/${orderId}`;
@@ -19,81 +19,85 @@ export default function OrderInfo({
   };
 
   return (
-    <div className="p-5 border-dark-green border rounded-xl">
-      {orderTracking.statuses.map((status, index) => {
-        if (status.status === "readyToSend") {
-          return (
-            <div key={index}>
-              <p>Commande validée !</p>
-              <p>
-                La commande est validée et en attente de livraison
-                {userType === "seller" && " de votre part"}.
-              </p>
-              <Button onClick={handleIsReceived}>
-                Confirmer la réception (test)
-              </Button>
-              {/* Pas à cette étape mais pour tester */}
-            </div>
-          );
-        }
-        if (status.status === "delivered") {
-          return (
-            <div>
-              <Button onClick={handleIsReceived}>Confirmer la réception</Button>
-              <p>Commande Livrée !</p>;
-            </div>
-          );
-        }
-        if (status.status === "shipping") {
-          return <p>Commande en cour de livraison !</p>;
-        }
-        if (status.status === "availableAtServicePoint") {
-          return <p>Disponible en point relais !</p>;
-        }
-        if (status.status === "late") {
-          return <p>Commande en retard !</p>;
-        }
-      })}
-
-      <div className="flex items-center mb-3">
-        <img src="/icons/delivery-check.svg" alt="" />
-        <div className="ml-5">
-          <p className="font-medium">Numéro de suivi colissimo créé</p>
-          <a
-            href={orderTracking.url}
-            target="_blank"
-            rel="noreferrer"
-            className="relative"
-          >
-            <span className="font-bold font-poppins">
-              {orderTracking.number}
-            </span>
-            <img
-              className="relative top-[-6px] ml-[2px] inline-block"
-              src="/icons/external-link.svg"
-              alt=""
-            />
-          </a>
-          <p className="text-sm font-poppins text-grey">
-            {ISOtoShortDate(orderTracking.createdAt)}
-          </p>
-        </div>
-      </div>
-      {orderTracking.statuses.map((status, index) => {
-        if (status.status === "readyToSend") {
-          return (
-            <div key={index} className="flex items-center">
-              <img src="/icons/delivery-check.svg" alt="" />
-              <div className="ml-5">
-                <p className="font-medium">Prêt à être livré</p>
-                <p className="text-sm font-poppins text-grey">
-                  {ISOtoShortDate(orderTracking.statuses[0].updatedAt)}
-                </p>
+    <>
+      <p>Commande validée !</p>
+      <p>
+        La commande est validée et en attente de livraison
+        {userType === "seller" && " de votre part"}.
+      </p>
+      <div className="p-5 border-dark-green border rounded-xl">
+        {orderTracking.statuses.map((status, index) => {
+          if (status.status === "readyToSend") {
+            return (
+              <div key={index}>
+                <Button onClick={handleIsReceived}>
+                  Confirmer la réception (test)
+                </Button>
+                {/* Pas à cette étape mais pour tester */}
               </div>
-            </div>
-          );
-        }
-      })}
-    </div>
+            );
+          }
+          if (status.status === "delivered") {
+            return (
+              <div>
+                <Button onClick={handleIsReceived}>
+                  Confirmer la réception
+                </Button>
+                <p>Commande Livrée !</p>;
+              </div>
+            );
+          }
+          if (status.status === "shipping") {
+            return <p>Commande en cour de livraison !</p>;
+          }
+          if (status.status === "availableAtServicePoint") {
+            return <p>Disponible en point relais !</p>;
+          }
+          if (status.status === "late") {
+            return <p>Commande en retard !</p>;
+          }
+        })}
+
+        <div className="flex items-center mb-3">
+          <img src="/icons/delivery-check.svg" alt="" />
+          <div className="ml-5">
+            <p className="font-medium">Numéro de suivi colissimo créé</p>
+            <a
+              href={orderTracking.url}
+              target="_blank"
+              rel="noreferrer"
+              className="relative"
+            >
+              <span className="font-bold font-poppins">
+                {orderTracking.number}
+              </span>
+              <img
+                className="relative top-[-6px] ml-[2px] inline-block"
+                src="/icons/external-link.svg"
+                alt=""
+              />
+            </a>
+            <p className="text-sm font-poppins text-grey">
+              {ISOtoShortDate(orderTracking.createdAt)}
+            </p>
+          </div>
+        </div>
+        {orderTracking.statuses.map((status, index) => {
+          if (status.status === "readyToSend") {
+            return (
+              <div key={index} className="flex items-center">
+                <img src="/icons/delivery-check.svg" alt="" />
+                <div className="ml-5">
+                  <p className="font-medium">Prêt à être livré</p>
+                  <p className="text-sm font-poppins text-grey">
+                    {ISOtoShortDate(orderTracking.statuses[0].updatedAt)}
+                  </p>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+    </>
   );
 }
