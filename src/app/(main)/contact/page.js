62a5cd171bf6eb7e708509e4f41d249ebe189d8b
Clément Lapasset <clement.lapasset@gmail.com>
2024-09-03 20:07:50 +0200

    add contacft page UI

--- src/app/(main)/contact/page.js
+++ src/app/(main)/contact/page.js
@@ -5,6 +5,11 @@ import { TextInput } from "@/components/input";
 import Button from "@/components/Button";
 import fetchHorseted from "@/utils/fetchHorseted";
 import { useAuthContext } from "@/context/AuthContext";
+import Breadcrumbs from "@/components/Breadcrumbs";
+import horsetedApp from "@/assets/images/horsetedApp.png";
+import Image from "next/image";
+import Link from "next/link";
+import Radio from "@/components/input/Radio";
 
 export default function Contact() {
   const { accessToken } = useAuthContext();
@@ -30,31 +35,65 @@ export default function Contact() {
     console.log("response =>", response);
   }
 
+  const breadcrumbs = [{ label: "Accueil", href: "/" }, { label: "Contact" }];
+
   return (
     <div>
-      <form
-        action={postContact}
-        className="form-container grid grid-cols-1 lg:grid-cols-2 gap-12 mb-5"
-      >
-        <TextInput
-          label="Object"
-          name="object"
-          value={contactData.object}
-          onChange={handleChange}
-          required
-          className="col-span-2 lg:col-span-1"
-        />
-        <TextInput
-          type="textarea"
-          label="Message"
-          name="message"
-          value={contactData.message}
-          onChange={handleChange}
-          required
-          className="col-span-2 lg:col-span-1"
-        />
-        <Button type="submit">Envoyer</Button>
-      </form>
+      <div className="bg-dark-green">
+        <div className="container mx-auto px-5 lg:px-0 pb-5 lg:pb-11">
+          <Breadcrumbs breadcrumbs={breadcrumbs} white />
+          <h1 className="text-4xl font-mcqueen font-bold text-white">
+            Contactez l'équipe Horseted
+          </h1>
+        </div>
+      </div>
+
+      <div className="container mx-auto py-11 px-5 lg:px-0">
+        <form
+          action={postContact}
+          className="form-container grid grid-cols-1 lg:grid-cols-2 gap-3 mb-5"
+        >
+          <TextInput
+            label="Objet"
+            name="object"
+            value={contactData.object}
+            onChange={handleChange}
+            required
+            className="col-span-2 lg:col-span-1 lg:w-1/2"
+            placeholder="Objet du message..."
+          />
+          <TextInput
+            type="textarea"
+            label="Message"
+            name="message"
+            value={contactData.message}
+            onChange={handleChange}
+            required
+            className="col-span-2 lg:col-span-1"
+            placeholder="Votre message..."
+          />
+          <div className="flex items-center gap-3 p-5 h-fit col-span-2 lg:col-span-1">
+            <Radio />
+            <p className="font-semibold text-sm">
+              J’accepte les{" "}
+              <Link className=" underline" href="/cgu">
+                conditions générales d’utilisation
+              </Link>
+            </p>
+          </div>
+          <Button className="col-span-2 lg:col-span-1" type="submit">
+            Envoyer
+          </Button>
+          <div className="hidden lg:flex lg:justify-end lg:col-start-2 lg:row-start-1 lg:row-span-4">
+            <Image
+              sizes="(min-width: 1024px) 920px, 50vw"
+              src={horsetedApp}
+              alt="App Horseted"
+              className="w-auto h-full "
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
