09e3b9e5eb3fd827a94f03c45118043c96452cec
Clément Lapasset <clement.lapasset@gmail.com>
2024-09-04 20:57:38 +0200

    fix state title in state select

--- src/app/(main)/vendre/selects/State.js
+++ src/app/(main)/vendre/selects/State.js
@@ -1,6 +1,33 @@
 import StateSelect from "../../articles/ProductFilters/StateSelect";
 
+const states = [
+  {
+    label: "Neuf avec emballage",
+    param: "new_with_packaging",
+  },
+  {
+    label: "Neuf sans emballage",
+    param: "new_without_packaging",
+  },
+  {
+    label: "Très bon état",
+    param: "perfect",
+  },
+  {
+    label: "Bon état",
+    param: "very_good",
+  },
+  {
+    label: "Mauvais état",
+    param: "bad",
+  },
+];
+
 export default function Category({ product, setProduct }) {
+  const translatedState =
+    states.find((state) => state.param === product.state)?.label ||
+    "État de l’article";
+
   const handleChange = (e) => {
     setProduct((prev) => ({ ...prev, state: e.target.value }));
   };
@@ -14,7 +41,7 @@ export default function Category({ product, setProduct }) {
         onStateChange={handleChange}
         activeState={product.state}
         required
-        title={product.state === "" ? "État de l’article" : product.state}
+        title={translatedState}
         className="w-full max-w-[700px]"
         isBlack
       />
