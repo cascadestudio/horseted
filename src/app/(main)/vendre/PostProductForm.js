444228e85687aa816c3ca7498205e6a0f49d7efc
Clément Lapasset <clement.lapasset@gmail.com>
2024-09-05 23:41:58 +0200

    fix sell product form styling and alert wording

--- src/app/(main)/vendre/PostProductForm.js
+++ src/app/(main)/vendre/PostProductForm.js
@@ -47,24 +47,26 @@ export default function PostProductForm({
 
   const validateForm = () => {
     let formErrors = {};
-    if (!product.title) formErrors.title = "Le titre est obligatoire.";
+    if (!product.title) formErrors.title = "Veuillez ajouter un titre.";
     if (!product.price || product.price <= 0)
-      formErrors.price = "Le prix doit être un nombre positif.";
+      formErrors.price = "Veuillez ajouter un prix supérieur à 0.";
     if (!product.description)
-      formErrors.description = "La description est obligatoire.";
+      formErrors.description = "Veuillez ajouter une description.";
     if (!product.categoryId)
-      formErrors.categoryId = "La catégorie est obligatoire.";
-    if (!product.sizeId) formErrors.sizeId = "La taille est obligatoire.";
-    if (!product.state) formErrors.state = "L'état est obligatoire.";
+      formErrors.categoryId = "Veuillez sélectionner une catégorie.";
+    if (!product.sizeId)
+      formErrors.sizeId = "Veuillez sélectionner une taille.";
+    if (!product.state)
+      formErrors.state = "Veuillez indiquer l'état du produit.";
     if (!product.shipping)
-      formErrors.shipping = "Le mode d'expédition est obligatoire.";
+      formErrors.shipping = "Veuillez choisir un mode d'expédition.";
     if (!product.materials || product.materials.length === 0)
-      formErrors.materials = "Les matériaux sont obligatoires.";
+      formErrors.materials = "Veuillez ajouter au moins un matériau.";
     if (!product.colors || product.colors.length === 0)
-      formErrors.colors = "Les couleurs sont obligatoires.";
+      formErrors.colors = "Veuillez ajouter au moins une couleur.";
     if (!product.medias.length)
-      formErrors.medias = "Veuillez ajouter au moins 1 photo";
-    if (!product.brand) formErrors.brand = "Veuillez ajouter au moins 1 photo";
+      formErrors.medias = "Veuillez ajouter au moins une photo.";
+    if (!product.brand) formErrors.brand = "Veuillez ajouter une marque.";
 
     setErrors(formErrors);
     return Object.keys(formErrors).length === 0;
@@ -95,7 +97,7 @@ export default function PostProductForm({
   return (
     <form
       onSubmit={handleFormSubmit}
-      className="container mx-auto p-16 bg-white rounded-3xl flex flex-col items-center gap-7"
+      className="container mx-auto p-16 bg-white rounded-3xl flex flex-col items-center gap-7 max-w-[1050px]"
     >
       <div className="relative">
         <ProductMedia accessToken={accessToken} setProduct={setProduct} />
@@ -113,12 +115,12 @@ export default function PostProductForm({
           onChange={handleFormChange}
           name="title"
           value={product.title}
-          className="mx-0 lg:mx-2"
+          className="mx-0 lg:mx-2 max-w-[700px]"
           hideLabel
           placeholder="Ex : Couverture de poney"
         />
         {errors.title && (
-          <p className="text-red text-xs absolute right-0 bottom-[-20px]">
+          <p className="text-red text-xs absolute right-2 bottom-[-20px]">
             {errors.title}
           </p>
         )}
@@ -131,20 +133,23 @@ export default function PostProductForm({
           onChange={handleFormChange}
           name="description"
           value={product.description}
-          className="mx-0 lg:mx-2"
+          className="mx-0 lg:mx-2 max-w-[700px]"
           hideLabel
           type="textarea"
           placeholder="Ex : Acheté le 10/12/2024, porté quelques fois mais ne me convient pas. Très bon état...poney"
         />
         {errors.description && (
-          <p className="text-red text-xs absolute right-0 bottom-[-20px]">
+          <p className="text-red text-xs absolute right-2 bottom-[-20px]">
             {errors.description}
           </p>
         )}
       </div>
       <div className="w-full flex flex-col lg:flex-row lg:justify-center">
         <h3 className="font-mcqueen font-semibold w-[200px]">Prix* :</h3>
-        <label className="font-mcqueen font-semibold w-full" htmlFor="price">
+        <label
+          className="font-mcqueen font-semibold w-full max-w-[700px]"
+          htmlFor="price"
+        >
           <div className="flex items-center border-b border-black relative">
             <input
               onChange={handleFormChange}
@@ -153,7 +158,7 @@ export default function PostProductForm({
               type="number"
               step="0.01"
               placeholder="Ex : 20"
-              className="focus:outline-none border-none bg-transparent w-full placeholder:text-grey pt-1 pb-2 resize-none overflow-hidden break-words whitespace-pre-wrap"
+              className="focus:outline-none border-none bg-transparent w-full placeholder:text-grey pt-1 pb-2 resize-none overflow-hidden break-words whitespace-pre-wrap "
             />
             <span className="text-xl font-mcqueen font-semibold mr-2">€</span>
             {errors.price && (
@@ -164,58 +169,58 @@ export default function PostProductForm({
           </div>
         </label>
       </div>
-      <div className="relative">
+      <div className="relative w-full">
         <Category product={product} setProduct={setProduct} />
         {errors.categoryId && (
-          <p className="text-red text-xs absolute right-0 bottom-[-20px]">
+          <p className="text-red text-xs absolute right-2 bottom-[-20px]">
             {errors.categoryId}
           </p>
         )}
       </div>
-      <div className="relative">
+      <div className="relative w-full">
         <State product={product} setProduct={setProduct} />
         {errors.state && (
-          <p className="text-red text-xs absolute right-0 bottom-[-20px]">
+          <p className="text-red text-xs absolute right-2 bottom-[-20px]">
             {errors.state}
           </p>
         )}
       </div>
-      <div className="relative">
+      <div className="relative w-full">
         <Size product={product} setProduct={setProduct} />
         {errors.size && (
-          <p className="text-red text-xs absolute right-0 bottom-[-20px]">
+          <p className="text-red text-xs absolute right-2 bottom-[-20px]">
             {errors.size}
           </p>
         )}
       </div>
-      <div className="relative">
+      <div className="relative w-full">
         <Colors product={product} setProduct={setProduct} />
         {errors.colors && (
-          <p className="text-red text-xs absolute right-0 bottom-[-20px]">
+          <p className="text-red text-xs absolute right-2 bottom-[-20px]">
             {errors.colors}
           </p>
         )}
       </div>
-      <div className="relative">
+      <div className="relative w-full">
         <Brand product={product} setProduct={setProduct} />
         {errors.brand && (
-          <p className="text-red text-xs absolute right-0 bottom-[-20px]">
+          <p className="text-red text-xs absolute right-2 bottom-[-20px]">
             {errors.brand}
           </p>
         )}
       </div>
-      <div className="relative">
+      <div className="relative w-full">
         <Materials product={product} setProduct={setProduct} />
         {errors.materials && (
-          <p className="text-red text-xs absolute right-0 bottom-[-20px]">
+          <p className="text-red text-xs absolute right-2 bottom-[-20px]">
             {errors.materials}
           </p>
         )}
       </div>
-      <div className="relative">
+      <div className="relative w-full">
         <Shipping product={product} setProduct={setProduct} />
         {errors.shipping && (
-          <p className="text-red text-xs absolute right-0 bottom-[-20px]">
+          <p className="text-red text-xs absolute right-2 bottom-[-20px]">
             {errors.shipping}
           </p>
         )}
