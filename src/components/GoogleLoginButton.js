ae5c4b3a6cbebedcc79d3bb52b0b75071647cb81
Clément Lapasset <clement.lapasset@gmail.com>
2024-09-09 17:00:43 +0200

    optimize google icon

--- src/components/GoogleLoginButton.js
+++ src/components/GoogleLoginButton.js
@@ -1,7 +1,5 @@
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "@/libs/firebase/config";
-import GoogleIcon from "@/assets/icons/GoogleIcon.svg";
-import Image from "next/image";
 import { postUser } from "@/utils/postUser";
 import { useRouter } from "next/navigation";
 import { getUser } from "@/utils/getUser";
@@ -46,13 +44,8 @@ export default function GoogleLoginButton({ type }) {
       onClick={handleGoogleSignIn}
       className="flex items-center border border-black w-fit rounded-[50px] p-1 ml-auto mr-auto lg:mb-[55px]"
     >
-      <div className="bg-white rounded-full h-[41px] w-[41px] flex items-center justify-center mr-3 lg:h-[50px] lg:w-[50px]">
-        <Image
-          src={GoogleIcon}
-          alt="Google Icon"
-          className="h-5 w-5 lg:h-6 lg:w-6"
-          priority
-        />
+      <div className="relative bg-white rounded-full h-[41px] w-[41px] flex items-center justify-center mr-3 lg:h-[50px] lg:w-[50px]">
+        <img src="/icons/google-logo.svg" alt="Logo Google" />
       </div>
       <span className="font-semibold pl-3 pr-8 lg:pr-[70px] lg:pl-[38px]">
         Continuer avec Google
