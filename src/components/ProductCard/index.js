00ed5a5356680f7a1942a44428508b7ada8b9f22
Clément Lapasset <clement.lapasset@gmail.com>
2024-09-09 12:00:23 +0200

    fix dragging carousel triggering click

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -38,29 +38,25 @@ export default function ProductCard({
   return (
     <div
       className={`
-      ${className} relative flex flex-col items-center border-b border-grey focus:outline-none"
+      ${className} flex flex-col items-center border-b border-grey focus:outline-none"
     `}
     >
-      <ClientProductImage product={product} className="w-[280px] h-[340px]" />
+      <Link href={`/product/${id}`}>
+        <ClientProductImage product={product} className="w-[280px] h-[340px]" />
+      </Link>
+
       <div className="flex w-full p-5 justify-between self-start">
-        <div className="max-w-[82%]">
+        <Link href={`/product/${id}`} className="max-w-[82%]">
           <p className="font-poppins font-bold">{centsToEuros(price)} €</p>
-          <Link href={`/product/${id}`} className="relative block">
-            <h4 className="text-lg font-extrabold text-light-green truncate relative">
-              {title}
-            </h4>
-          </Link>
+          <h4 className="text-lg font-extrabold text-light-green truncate">
+            {title}
+          </h4>
           <p className="text-grey">{shippingSizeFrench}</p>
-        </div>
+        </Link>
         <div className="flex items-start">
           <FavoriteButton favoriteCount={favoritCount} productId={id} />
         </div>
       </div>
-      <Link
-        href={`/product/${id}`}
-        className="absolute inset-0 z-10"
-        aria-label={`View ${title}`}
-      />
     </div>
   );
 }
