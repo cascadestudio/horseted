195d70538e74ec4d5e1c9a9765c5d9491afae599
AdrienLapasset <aflapasset@gmail.com>
2024-09-12 13:38:15 +0200

    Add empty product and reviews messages

--- src/components/ProfilePage/ProfileTabs.js
+++ src/components/ProfilePage/ProfileTabs.js
@@ -1,15 +1,17 @@
+import { useAuthContext } from "@/context/AuthContext";
 import { useEffect, useState } from "react";
 import ProductCard from "@/components/ProductCard";
 import ReviewCard from "@/components/ReviewCard";
 import fetchHorseted from "@/utils/fetchHorseted";
 import StarRating from "@/components/StarRating";
+import { usePathname } from "next/navigation";
 
-export default function ProfileTabs({ profile, accessToken }) {
+export default function ProfileTabs({ profile }) {
+  const { accessToken } = useAuthContext();
   const [activeTab, setActiveTab] = useState("products");
   const [products, setProducts] = useState([]);
   const [reviews, setReviews] = useState({});
-
-  //console.log("reviews =>", reviews);
+  const pathname = usePathname();
 
   useEffect(() => {
     if (profile) {
@@ -37,68 +39,115 @@ export default function ProfileTabs({ profile, accessToken }) {
     rating: reviews?.rating,
   };
 
-  if (!products.length && !reviews?.reviews?.length) return null;
-
   return (
     <div className="mt-8">
       <div className="flex border-b border-lighter-grey">
-        {products.length && (
-          <button
-            className={`px-4 py-2 text-lg font-medium font-mcqueen ${
-              activeTab === "products"
-                ? "text-light-green border-b-[3px] border-light-green"
-                : "text-grey"
-            }`}
-            onClick={() => setActiveTab("products")}
-          >
-            Sellerie
-          </button>
-        )}
-        {reviews?.reviews?.length > 0 && (
-          <button
-            className={`px-4 py-2 text-lg font-medium font-mcqueen ${
-              activeTab === "reviews"
-                ? "text-light-green border-b-[3px] border-light-green"
-                : "text-grey"
-            }`}
-            onClick={() => setActiveTab("reviews")}
-          >
-            Évaluations
-          </button>
-        )}
+        <button
+          className={`px-4 py-2 text-lg font-medium font-mcqueen ${
+            activeTab === "products"
+              ? "text-light-green border-b-[3px] border-light-green"
+              : "text-grey"
+          }`}
+          onClick={() => setActiveTab("products")}
+        >
+          Sellerie
+        </button>
+        <button
+          className={`px-4 py-2 text-lg font-medium font-mcqueen ${
+            activeTab === "reviews"
+              ? "text-light-green border-b-[3px] border-light-green"
+              : "text-grey"
+          }`}
+          onClick={() => setActiveTab("reviews")}
+        >
+          Évaluations
+        </button>
       </div>
       <div className="mt-4">
-        {activeTab === "products" && (
-          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-full gap-14 py-12">
-            {products.map((product, index) => (
-              <div key={index}>
-                <ProductCard product={product} />
-              </div>
-            ))}
-          </section>
-        )}
-        {activeTab === "reviews" && (
-          <div>
-            <div className="flex flex-col items-center justify-center border-b border-lighter-grey mb-8 pb-5">
-              <p className="font-mcqueen font-semibold text-[40px] leading-[48px]">
-                {reviews.rating}
-              </p>
-              <p className="font-mcqueen">
-                {reviews.reviews.length} évaluations
-              </p>
-              <StarRating review={globalReview} size="sm" />
-            </div>
-            <div className="columns-1 sm:columns-2 lg:columns-3 gap-3">
-              {reviews.reviews.map((review, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col items-center break-inside-avoid mb-3"
-                >
-                  <ReviewCard review={review} />
-                </div>
-              ))}
-            </div>
-          </div>
+        {activeTab === "products" ? (
+          <>
+            {products.length > 0 && (
+              <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-full gap-14 py-12">
+                {products.map((product, index) => (
+                  <div key={index}>
+                    <ProductCard product={product} />
+                  </div>
+                ))}
+              </section>
+            )}
+            {products.length === 0 && (
+              <section className="flex flex-col items-center justify-center text-center gap-7 max-w-[1050px] pt-8">
+                <img
+                  src="/images/baby-horse.svg"
+                  alt="Illustration bébé cheval"
+                />
+                <h1 className="font-mcqueen font-bold text-3xl">
+                  Aucune vente en cours
+                </h1>
+
+                {pathname.startsWith("/mon-compte") && (
+                  <p className="max-w-[475px] mb-6">
+                    Vous ne proposez aucun produit à la vente pour le moment
+                  </p>
+                )}
+                {pathname.startsWith("/vendeur") && (
+                  <p className="max-w-[475px] mb-6">
+                    Ce vendeur ne propose pas encore de produit
+                  </p>
+                )}
+              </section>
+            )}
+          </>
+        ) : (
+          activeTab === "reviews" && (
+            <>
+              {reviews?.reviews?.length > 0 && (
+                <>
+                  <div className="flex flex-col items-center justify-center border-b border-lighter-grey mb-8 pb-5">
+                    <p className="font-mcqueen font-semibold text-[40px] leading-[48px]">
+                      {reviews.rating}
+                    </p>
+                    <p className="font-mcqueen">
+                      {reviews.reviews.length} évaluations
+                    </p>
+                    <StarRating review={globalReview} size="sm" />
+                  </div>
+                  <div className="columns-1 sm:columns-2 lg:columns-3 gap-3">
+                    {reviews.reviews.map((review, index) => (
+                      <div
+                        key={index}
+                        className="flex flex-col items-center break-inside-avoid mb-3"
+                      >
+                        <ReviewCard review={review} />
+                      </div>
+                    ))}
+                  </div>
+                </>
+              )}
+              {reviews?.reviews?.length === 0 && (
+                <section className="flex flex-col items-center justify-center text-center gap-7 max-w-[1050px] pt-8">
+                  <img
+                    src="/images/baby-horse.svg"
+                    alt="Illustration bébé cheval"
+                  />
+                  <h1 className="font-mcqueen font-bold text-3xl">
+                    Aucune évaluation
+                  </h1>
+                  {pathname.startsWith("/mon-compte") && (
+                    <p className="max-w-[475px] mb-6">
+                      Commencer à acheter ou vendre un article pour recevoir une
+                      évaluation
+                    </p>
+                  )}
+                  {pathname.startsWith("/vendeur") && (
+                    <p className="max-w-[475px] mb-6">
+                      Ce vendeur n'a pas encore reçu d'évaluation
+                    </p>
+                  )}
+                </section>
+              )}
+            </>
+          )
         )}
       </div>
     </div>
