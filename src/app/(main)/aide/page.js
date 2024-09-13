5e0918caffc0736c676a9ec8a280c170e07ae093
Clément Lapasset <clement.lapasset@gmail.com>
2024-09-13 13:34:46 +0200

    add help center UI

--- src/app/(main)/aide/page.js
+++ src/app/(main)/aide/page.js
@@ -1,9 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { groq } from "next-sanity";
 import { client } from "../../../../sanity/lib/client";
 import Breadcrumbs from "@/components/Breadcrumbs";
+import ChevronRight from "@/assets/icons/ChevronRight";
 
 const helpCategoriesQuery = groq`
   *[_type == "helpCategory"]{
@@ -20,7 +21,6 @@ export default function HelpCenter() {
   const [categories, setCategories] = useState([]);
   const [activeCategory, setActiveCategory] = useState(null);
   const pathname = usePathname();
-  const router = useRouter();
 
   useEffect(() => {
     const fetchHelpCategories = async () => {
@@ -40,79 +40,56 @@ export default function HelpCenter() {
   };
 
   const breadcrumbs = () => {
-    const base = [
-      {
-        label: "Accueil",
-        href: "/",
-      },
-    ];
-    if (!activeCategory) {
-      return [
-        ...base,
-        {
-          label: "Centre d'aide",
-        },
-      ];
-    } else {
-      return [
-        ...base,
-        {
-          label: "Centre d'aide",
-          href: "/aide",
-        },
-        {
-          label: activeCategory.title,
-        },
-      ];
-    }
+    return [{ label: "Accueil", href: "/" }, { label: "Centre d'aide" }];
   };
 
   return (
-    <div className="container mx-auto px-5 grid grid-cols-3 gap-4 lg:gap-14 pt-11 pb-16">
+    <div className="container mx-auto px-5 grid grid-cols-3 gap-4 lg:gap-14 pb-16">
       <div className="col-span-3 lg:col-span-1">
         <Breadcrumbs breadcrumbs={breadcrumbs()} />
-        <h1 className="font-mcqueen font-bold text-4xl mb-5">
-          {activeCategory ? activeCategory.title : "Centre d'aide"}
-        </h1>
+        <h1 className="font-mcqueen font-bold text-4xl mb-5">Centre d'aide</h1>
         <aside className="bg-light-grey flex flex-col border border-lighter-grey">
           {categories.map((category, index) => (
-            <div key={index} className="p-4">
-              <h2
-                className="font-semibold cursor-pointer text-blue-600 hover:underline"
-                onClick={() => handleCategoryClick(category)}
-              >
-                {category.title}
-              </h2>
+            <div
+              key={index}
+              className={`cursor-pointer border-lighter-grey py-5 px-9 ${
+                activeCategory &&
+                activeCategory.slug.current === category.slug.current
+                  ? "font-extrabold border-y bg-pale-grey"
+                  : "font-medium"
+              }`}
+              onClick={() => handleCategoryClick(category)}
+            >
+              <h2>{category.title}</h2>
             </div>
           ))}
         </aside>
       </div>
 
-      <div className="col-span-3 lg:col-span-2">
-        {activeCategory ? (
+      <div className="col-span-3 lg:col-span-2 pt-32">
+        {activeCategory && (
           <div>
-            <h2 className="text-2xl font-bold mb-4">
-              Articles in {activeCategory.title}
-            </h2>
             <ul>
               {activeCategory.articles.length > 0 ? (
                 activeCategory.articles.map((article, i) => (
-                  <li key={i} className="mb-2">
+                  <li
+                    key={i}
+                    className="font-medium border-b border-lighter-grey"
+                  >
                     <a
-                      className="text-blue-600 hover:underline"
                       href={`/aide/${activeCategory.slug.current}/${article.slug.current}`}
+                      className="w-full py-4 px-6 flex items-center justify-between hover:bg-pale-grey"
                     >
                       {article.title}
+                      <ChevronRight />
                     </a>
                   </li>
                 ))
               ) : (
-                <p>No articles available in this category.</p>
+                <p>Pas d'article dans cette catégorie</p>
               )}
             </ul>
           </div>
-        ) : (
-          <p>Please select a category to view articles.</p>
         )}
       </div>
     </div>
