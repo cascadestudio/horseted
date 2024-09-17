d8075bad4cfcd408967da7c39adbed6c6cda5e78
Clément Lapasset <clement.lapasset@gmail.com>
2024-09-17 11:46:18 +0200

    truncate breadcrumb

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -3,18 +3,19 @@ import Link from "next/link";
 const Breadcrumbs = ({ breadcrumbs, white }) => {
   return (
     <nav className="flex pt-10 pb-2" aria-label="Breadcrumb">
-      <ol className="inline-flex items-center">
+      <ol className="inline-flex items-center w-full overflow-hidden sm:overflow-visible sm:text-nowrap">
         {breadcrumbs.map((breadcrumb, index) => (
-          <li key={index} className="font-medium">
+          <li
+            key={index}
+            className="font-medium max-w-full inline-block sm:max-w-none"
+          >
             {index !== breadcrumbs.length - 1 ? (
               <>
                 <Link
                   href={breadcrumb.href}
-                  className={
-                    white
-                      ? "text-white underline underline-offset-2 whitespace-nowrap"
-                      : "text-light-green underline underline-offset-2 whitespace-nowrap"
-                  }
+                  className={`${
+                    white ? "text-white " : "text-light-green"
+                  } underline underline-offset-2 truncate`}
                 >
                   {breadcrumb.label}
                 </Link>
@@ -24,11 +25,7 @@ const Breadcrumbs = ({ breadcrumbs, white }) => {
               </>
             ) : (
               <span
-                className={
-                  white
-                    ? "text-white whitespace-nowrap"
-                    : "text-black whitespace-nowrap"
-                }
+                className={`${white ? "text-white" : "text-black"} line-clamp-1 sm:line-clamp-none`}
               >
                 {breadcrumb.label}
               </span>
