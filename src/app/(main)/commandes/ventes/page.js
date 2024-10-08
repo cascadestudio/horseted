7481a8ad71d94e8e864af7d7f0cc960cc1a5a8a2
Clément Lapasset <clement.lapasset@gmail.com>
2024-10-08 11:23:11 +0200

    add metadata to commandes page

--- src/app/(main)/commandes/ventes/page.js
+++ src/app/(main)/commandes/ventes/page.js
@@ -1,5 +1,20 @@
 import OrderList from "../OrderList";
 
+export async function generateMetadata() {
+  const title = "Commandes | Horseted";
+  const description =
+    "Consultez vos commandes effectués sur Horseted. Retrouvez ici toutes vos commandes récentes et passées.";
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
+  };
+}
+
 export default function Ventes() {
   return <OrderList orderType="sale" />;
 }
