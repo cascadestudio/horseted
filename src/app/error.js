1c16ed9a301d1594acf0497f3783f8865075723a
AdrienLapasset <aflapasset@gmail.com>
2024-10-16 16:37:35 +0200

    add cookie consent

--- src/app/error.js
+++ src/app/error.js
@@ -5,7 +5,7 @@ import Button from "@/components/Button";
 export default function NotFound() {
   return (
     <>
-      <div className="bg-light-grey">
+      <div className="bg-light-grey h-[100vh]">
         <div className="container mx-auto px-5 py-24 flex flex-col lg:py-44 items-center justify-center">
           <h1 className="font-mcqueen font-bold text-xl leading-[48px] lg:mb-3 lg:text-5xl ">
             Erreur
